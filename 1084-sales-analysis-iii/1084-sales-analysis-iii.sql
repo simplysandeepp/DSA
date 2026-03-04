e6161422eb0665aeab7fc91d85128590bd5c67f4
Sandeep Prajapati <139686638+simplysandeepp@users.noreply.github.com>
2026-03-04 20:36:49 +0530

    Time: 970 ms (43.4%), Space: 0B (100%) - LeetHub

---
+++ 1084-sales-analysis-iii/1084-sales-analysis-iii.sql
@@ -0,0 +1,6 @@
+SELECT  p.product_id, p.product_name
+FROM Product p
+JOIN Sales s
+ON p.product_id = s.product_id 
+GROUP BY p.product_id, p.product_name
+HAVING MIN(sale_date) >= '2019-01-01' AND MAX(sale_date) <= '2019-03-31';
