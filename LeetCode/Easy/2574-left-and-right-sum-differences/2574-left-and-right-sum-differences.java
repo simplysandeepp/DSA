542394f588a8db966d10faf5937cd0ca4b245721
Sandeep Prajapati <139686638+simplysandeepp@users.noreply.github.com>
2026-03-12 11:06:27 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- LeetCode/Easy/2574-left-and-right-sum-differences/2574-left-and-right-sum-differences.java
+++ LeetCode/Easy/2574-left-and-right-sum-differences/2574-left-and-right-sum-differences.java
@@ -7,13 +7,13 @@ class Solution {
         int[] r = new int[n];
         int[] ans = new int[n];
 
-        for(int i=1; i<n; i++) {
+        for(int i=1; i<n; i++) { //prefix sum
             l[i] = l[i-1] + nums[i-1];
         }
-        for(int i=n-2; i>=0; i--) {
+        for(int i=n-2; i>=0; i--) { // suffex sum
             r[i] = r[i+1] + nums[i+1];
         }
-        for(int i=0;i<n;i++){
+        for(int i=0;i<n;i++){ //stroing diff
             ans[i] =Math.abs(l[i]-r[i]);
         }
         return ans;
