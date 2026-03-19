c4f4dc351acb353d316bd7e896cc48c7fc55a075
Sandeep Prajapati <139686638+simplysandeepp@users.noreply.github.com>
2026-03-19 10:27:44 +0530

    Time: 152 ms (27.77%), Space: 108.5 MB (28.52%) - LeetHub

--- LeetCode/Hard/0315-count-of-smaller-numbers-after-self/0315-count-of-smaller-numbers-after-self.java
+++ LeetCode/Hard/0315-count-of-smaller-numbers-after-self/0315-count-of-smaller-numbers-after-self.java
@@ -1,29 +1,67 @@
 class Solution {
-    int[] tree;
-    int size = 20002;
-    public List<Integer> countSmaller(int[] nums) {
-        tree = new int[size];
-        Integer[] res = new Integer[nums.length];
 
-        for (int i = nums.length - 1; i >= 0; i--) {
-            int index = nums[i] + 10001;
-            res[i] = query(index - 1);
-            update(index);
+    static class SegTree {
+        int n;
+        int[]tree;
+        public SegTree(int n) {
+            this.n = n;
+            tree = new int[4*n];
         }
-        return Arrays.asList(res);
+        public int query(int node, int L, int R, int ql, int qr) {
+            // no overlapping
+            if(L > qr || R < ql) {
+                return 0;
+            }
+            // fully overlapping
+            if(ql <= L && R <= qr) {
+                return tree[node];
+            }
+            // partial
+            int mid = (L+R)/2 ;
+
+            return query(2*node+1, L, mid, ql, qr) + query(2*node+2, mid+1, R, ql, qr);
+        }
+
+        public void update (int node, int L, int R, int idx) {
+            if(L == R) {
+                tree[node]++;
+                return;
+            }
+            int mid = (L+R)/2;
+            if(idx <= mid) {
+                update(2*node+1, L, mid, idx);
+            } 
+            else {
+                update(2*node+2, mid+1, R, idx);
+            }
+            tree[node] = tree[2*node+1] + tree[2*node+2];
+        }
     }
-    private void update(int i) {
-        while (i < size) {
-            tree[i] += 1;
-            i += i & (-i);
+    public List<Integer> countSmaller(int[] nums) {
+        int n = nums.length;
+        int[] sorted = nums.clone();
+        Arrays.sort(sorted);
+
+        HashMap<Integer, Integer> map = new HashMap<>();
+        int idx = 0;
+
+        for(int val : sorted) {
+            if(!map.containsKey(val)) {
+                map.put(val,idx++);
+            }
         }
-    }
-    private int query(int i) {
-        int sum = 0;
-        while (i > 0) {
-            sum += tree[i];
-            i -= i & (-i);
+
+        //yaha hum segment tree la logic likhenge
+
+        SegTree seg = new SegTree(idx);
+        Integer [] ans = new Integer[n];
+
+        //right ->left
+        for(int i=n-1; i>=0; i--) {
+            int val = map.get(nums[i]);
+            ans[i] = seg.query(0, 0, idx-1, 0, val-1);  // ndoe, L, R, ql, qr
+            seg.update(0, 0, idx-1, val);
         }
-        return sum;
+        return Arrays.asList(ans);
     }
 }
