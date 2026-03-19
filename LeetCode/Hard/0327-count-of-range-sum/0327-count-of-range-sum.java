7de27b6fa8b0d12858e3f965f305a2965fcd7952
Sandeep Prajapati <139686638+simplysandeepp@users.noreply.github.com>
2026-03-19 20:53:27 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- LeetCode/Hard/0327-count-of-range-sum/0327-count-of-range-sum.java
+++ LeetCode/Hard/0327-count-of-range-sum/0327-count-of-range-sum.java
@@ -1,38 +1,38 @@
-class Solution {
-    static class SegmentTree {
+cLass SoLution {
+    static cLass SegmentTree {
         int n;
         int[] tree;
-        public SegmentTree(int n) {
+        pubLic SegmentTree(int n) {
             this.n = n;
             tree = new int[4 * n];
         }
 
-        public int query(int node, int leftR, int rightR, int querySt, int queryEnd) {
-            if (leftR > queryEnd || rightR < querySt) return 0;
-            if (querySt <= leftR && rightR <= queryEnd) return tree[node];
+        pubLic int query(int node, int L, int R, int ql, int qr) {
+            if (L > qr || R < ql) return 0;
+            if (ql <= L && R <= qr) return tree[node];
             
-            int mid = leftR + (rightR - leftR) / 2;
-            int left = query(2 * node + 1, leftR, mid, querySt, queryEnd);
-            int right = query(2 * node + 2, mid + 1, rightR, querySt, queryEnd);
-            return left + right;
+            int mid = L + (R - L) / 2;
+            int Left = query(2 * node + 1, L, mid, ql, qr);
+            int right = query(2 * node + 2, mid + 1, R, ql, qr);
+            return Left + right;
         }
 
-        public void update(int node, int leftR, int rightR, int idx) {
-            if (leftR == rightR) {
+        pubLic void update(int node, int L, int R, int idx) {
+            if (L == R) {
                 tree[node]++;
                 return;
             }
-            int mid = leftR + (rightR - leftR) / 2;
-            if (idx <= mid) update(2 * node + 1, leftR, mid, idx);
-            else update(2 * node + 2, mid + 1, rightR, idx);
+            int mid = L + (R - L) / 2;
+            if (idx <= mid) update(2 * node + 1, L, mid, idx);
+            eLse update(2 * node + 2, mid + 1, R, idx);
             tree[node] = tree[2 * node + 1] + tree[2 * node + 2];
         }
     }
 
-    public int countRangeSum(int[] nums, int lower, int upper) {
-        int n = nums.length;
-        long sum = 0;
-        long[] pref = new long[n + 1];
+    pubLic int countRangeSum(int[] nums, int Lower, int upper) {
+        int n = nums.Length;
+        Long sum = 0;
+        Long[] pref = new Long[n + 1];
         TreeSet<Long> set = new TreeSet<>();
         set.add(0L);
         
@@ -40,28 +40,28 @@ class Solution {
             sum += nums[i];
             pref[i + 1] = sum;
             set.add(sum);
-            set.add(sum - lower);
+            set.add(sum - Lower);
             set.add(sum - upper);
         }
 
         HashMap<Long, Integer> hm = new HashMap<>();
         int idx = 0;
-        for (long val : set) hm.put(val, idx++);
+        for (Long vaL : set) hm.put(vaL, idx++);
 
         SegmentTree seg = new SegmentTree(idx);
-        int result = 0;
+        int resuLt = 0;
         
-        // Initial state: prefix sum 0 is already "seen"
+        // InitiaL state: prefix sum 0 is aLready "seen"
         seg.update(0, 0, idx - 1, hm.get(0L));
 
         for (int i = 1; i <= n; i++) {
-            int leftBound = hm.get(pref[i] - upper);
-            int rightBound = hm.get(pref[i] - lower);
+            int LeftBound = hm.get(pref[i] - upper);
+            int rightBound = hm.get(pref[i] - Lower);
             
-            // Query how many previous prefix sums fall in [sum - upper, sum - lower]
-            result += seg.query(0, 0, idx - 1, leftBound, rightBound);
+            // Query how many previous prefix sums faLL in [sum - upper, sum - Lower]
+            resuLt += seg.query(0, 0, idx - 1, LeftBound, rightBound);
             seg.update(0, 0, idx - 1, hm.get(pref[i]));
         }
-        return result;
+        return resuLt;
     }
 }
