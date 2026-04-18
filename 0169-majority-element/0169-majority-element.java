a4f34d29b29fc297bd84c1e55199b5b4fb8ec0d8
Sandeep Prajapati <139686638+simplysandeepp@users.noreply.github.com>
2026-04-18 20:38:31 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0169-majority-element/0169-majority-element.java
+++ 0169-majority-element/0169-majority-element.java
@@ -1,15 +1,23 @@
+// class Solution {
+//     public int majorityElement(int[] nums) {
+//         HashMap<Integer, Integer> map = new HashMap<>();
+//         int n = nums.length;
+//         for(int num : nums) {
+//             map.put(num, map.getOrDefault(num, 0) + 1);
+//         }
+//         for(int x : nums) {
+//             if(map.get(x) > n/2) {
+//                 return x;
+//             }
+//         }
+//         return -1;
+//     }
+// }
+
 class Solution {
     public int majorityElement(int[] nums) {
-        HashMap<Integer, Integer> map = new HashMap<>();
+        Arrays.sort(nums);
         int n = nums.length;
-        for(int num : nums) {
-            map.put(num, map.getOrDefault(num, 0) + 1);
-        }
-        for(int x : nums) {
-            if(map.get(x) > n/2) {
-                return x;
-            }
-        }
-        return -1;
+        return nums[n/2];
     }
 }
