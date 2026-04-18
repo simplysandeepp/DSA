226248daa09887255cd7bf84afeae86a4352eee0
Sandeep Prajapati <139686638+simplysandeepp@users.noreply.github.com>
2026-04-18 23:09:24 +0530

    Time: 1 ms (100%), Space: 43.9 MB (21.72%) - LeetHub

--- 0010-regular-expression-matching/0010-regular-expression-matching.java
+++ 0010-regular-expression-matching/0010-regular-expression-matching.java
@@ -1,35 +1,26 @@
-enum Result {
-    TRUE, FALSE
-}
-
 class Solution {
-    Result[][] memo;
-
-    public boolean isMatch(String text, String pattern) {
-        memo = new Result[text.length() + 1][pattern.length() + 1];
-        return dp(0, 0, text, pattern);
+    public boolean isMatch(String s, String p) {
+        int m = s.length();
+        int n = p.length();
+        
+        Boolean[][] dp = new Boolean[m + 1][n + 1];
+        return solve(0, 0, s, p, dp);
     }
 
-    public boolean dp(int i, int j, String text, String pattern) {
-        if (memo[i][j] != null) {
-            return memo[i][j] == Result.TRUE;
+    public boolean solve(int i, int j, String s, String p, Boolean[][] dp) {
+        if (dp[i][j] != null) return dp[i][j];
+        if (j == p.length()) {
+            return dp[i][j] = (i == s.length());
         }
-        boolean ans;
-        if (j == pattern.length()){
-            ans = i == text.length();
-        } else{
-            boolean first_match = (i < text.length() &&
-                                   (pattern.charAt(j) == text.charAt(i) ||
-                                    pattern.charAt(j) == '.'));
 
-            if (j + 1 < pattern.length() && pattern.charAt(j+1) == '*'){
-                ans = (dp(i, j+2, text, pattern) ||
-                       first_match && dp(i+1, j, text, pattern));
-            } else {
-                ans = first_match && dp(i+1, j+1, text, pattern);
-            }
+        boolean match = (i <s.length() && (s.charAt(i) == p.charAt(j) || p.charAt(j)== '.'));
+        if (j+1 < p.length() && p.charAt(j+1) == '*') {
+            return dp[i][j] = (solve(i, j + 2, s, p, dp) || (match && solve(i + 1, j, s, p, dp)));
         }
-        memo[i][j] = ans ? Result.TRUE : Result.FALSE;
-        return ans;
+        //normal caes
+        if (match) {
+            return dp[i][j] = solve(i + 1, j + 1, s, p, dp);
+        }
+        return dp[i][j] = false;
     }
 }
