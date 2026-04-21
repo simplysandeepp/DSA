84bfb2648aeef1bc68da079c0c7c5611f87db931
Sandeep Prajapati <139686638+simplysandeepp@users.noreply.github.com>
2026-04-21 20:41:04 +0530

    Time: 31 ms (81.22%), Space: 44 MB (72.92%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.java
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.java
@@ -1,24 +1,24 @@
 class Solution {
     public boolean canPartition(int[] nums) {
-        int n = nums.length;
         int sum = 0;
-        for (int num : nums) sum += num;
-        if (sum % 2 != 0) return false; 
-        int target = sum / 2;
-
-        boolean[][] dp = new boolean[n][target + 1];
+        for (int n : nums){
+            sum +=n; 
+        }
         
-        for (int i = 0; i < n; i++) dp[i][0] = true;         
-        if (nums[0] <= target) dp[0][nums[0]] = true;
+        if (sum%2 != 0){
+            return false;
+        }
 
-        for (int i = 1; i < n; i++) {
-            for (int t = 1; t <= target; t++) {
-                boolean notTake = dp[i - 1][t];
-                boolean take = false;
-                if (nums[i] <= t) take = dp[i - 1][t - nums[i]];
-                dp[i][t] = take || notTake;
+        int half = sum/2;
+        boolean[] dp = new boolean[half+1];
+        dp[0] = true;
+
+        for (int num : nums) {
+            for (int j = half; j >= num; j--) {
+                dp[j] = dp[j] || dp[j - num];
             }
         }
-        return dp[n - 1][target];
+
+        return dp[half];
     }
 }
