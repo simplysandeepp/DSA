012c0d3d64469ddc921f3ef849646409e5075c12
Sandeep Prajapati <139686638+simplysandeepp@users.noreply.github.com>
2026-04-24 12:26:06 +0530

    Time: 0 ms (100%), Space: 47.1 MB (12.53%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.java
+++ 0733-flood-fill/0733-flood-fill.java
@@ -1,21 +1,29 @@
 class Solution {
     public int[][] floodFill(int[][] image, int sr, int sc, int color) {
         int real = image[sr][sc];
-        if(real == color) {
-            return image;
-        }
-        dfs(image, sr, sc, real, color);
+        if (real == color) return image;
+        dfs(image,sr,sc,real,color);
         return image;
     }
+
     void dfs(int[][] image, int r, int c, int real, int color) {
-        if (r < 0 || c < 0 || r >= image.length || c >= image[0].length)
+        // boundary check
+        if (r<0 || c<0 || r >= image.length || c >= image[0].length)
             return;
+
+        //different color check
         if (image[r][c] != real)
             return;
+
+        // changing its color
         image[r][c] = color;
-        dfs(image,r+1,c,real,color); 
-        dfs(image,r-1,c,real,color); 
-        dfs(image,r,c+1,real,color); 
-        dfs(image,r,c-1,real,color); 
+
+        // all four directions
+        int[][] dir = {{1,0}, {-1,0}, {0,1}, {0,-1}};
+        for (int i = 0; i < 4; i++) {
+            int x = r + dir[i][0];
+            int y = c + dir[i][1];
+            dfs(image, x, y, real, color);
+        }
     }
 }
