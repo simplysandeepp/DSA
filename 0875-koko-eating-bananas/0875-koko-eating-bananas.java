1a38ef52bd5a0d08fa1a43ff0a4f2bb311b1fd42
Sandeep Prajapati <139686638+simplysandeepp@users.noreply.github.com>
2026-04-29 10:50:03 +0530

    Time: 6 ms (99.72%), Space: 48 MB (42.56%) - LeetHub

--- 0875-koko-eating-bananas/0875-koko-eating-bananas.java
+++ 0875-koko-eating-bananas/0875-koko-eating-bananas.java
@@ -1,27 +1,33 @@
-public class Solution {
+class Solution {
     public int minEatingSpeed(int[] piles, int h) {
-        int left = 1;  
-        int right = Arrays.stream(piles).max().getAsInt();
-        int ans = right;
+        int l = 1;
+        int high = Integer.MIN_VALUE;
+        for (int i = 0; i < piles.length; i++) {
+            high = Math.max(high, piles[i]);
+        }
 
-        while (left <= right) {
-            int mid = left + (right - left) / 2;
-            if (canFinish(piles, h, mid)) {
-                ans = mid;     
-                right = mid - 1;
+        int ans = high;
+        while (l <= high) {
+            int mid = l + (high - l) / 2;
+
+            if (isPossible(mid, h, piles)) {
+                ans = mid;        
+                high = mid - 1;   
             } else {
-                left = mid + 1; 
+                l = mid + 1;    
             }
         }
+
         return ans;
     }
 
-    public boolean canFinish(int[] piles, int h, int k) {
-        long hours = 0;
-        for (int pile : piles) {
-            hours += pile / k;
-            if (pile % k != 0) hours++;
+    public boolean isPossible(int mid, int h, int[] arr) {
+        long totalHours = 0;
+
+        for (int i = 0; i < arr.length; i++) {
+            totalHours += (arr[i] + mid - 1) / mid; 
         }
-        return hours <= h;
+
+        return totalHours <= h;
     }
 }
