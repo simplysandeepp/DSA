ebb8ab151603911fb473e2199b014945a6da5148
Sandeep Prajapati <139686638+simplysandeepp@users.noreply.github.com>
2026-05-02 00:23:17 +0530

    Time: 14 ms (89.18%), Space: 93.8 MB (26.3%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.java
+++ 0217-contains-duplicate/0217-contains-duplicate.java
@@ -1,13 +1,25 @@
+// class Solution {
+//     public boolean containsDuplicate(int[] nums) {
+//         Arrays.sort(nums);
+//         int j = 1;
+//         for(int i=0; i<nums.length-1; i++){
+//             if(nums[i] == nums[j]){
+//                 return true;
+//             } else {
+//                 j++;
+//             }
+//         } return false;
+//     }
+// }
+
 class Solution {
     public boolean containsDuplicate(int[] nums) {
-        Arrays.sort(nums);
-        int j = 1;
-        for(int i=0; i<nums.length-1; i++){
-            if(nums[i] == nums[j]){
-                return true;
-            } else {
-                j++;
-            }
-        } return false;
+        HashSet<Integer> s = new HashSet<>();
+        for(int num : nums){
+            // duplicate found 
+            if(s.contains(num)) return true; 
+            s.add(num);
+        }
+        return false;
     }
 }
