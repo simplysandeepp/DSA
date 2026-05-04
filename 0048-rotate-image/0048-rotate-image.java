81036818c34e21e8c694f91b4733607533c1ed97
Sandeep Prajapati <139686638+simplysandeepp@users.noreply.github.com>
2026-05-04 21:42:49 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0048-rotate-image/0048-rotate-image.java
+++ 0048-rotate-image/0048-rotate-image.java
@@ -1,7 +1,6 @@
 class Solution {
     public void rotate(int[][] matrix) {
         int n=matrix.length;
-
         for(int i=0;i<n;i++){
             for(int j=i;j<n;j++){
                 int temp=matrix[i][j];
@@ -9,11 +8,9 @@ class Solution {
                 matrix[j][i]=temp;
             }
         }
-        
         for(int i=0;i<n;i++){
             int start=0;
             int end=n-1;
-        
             while(start<end){
                 int temp=matrix[i][start];
                 matrix[i][start]=matrix[i][end];
